8d079420195c9c2f76b3ea31dd15d0db8892c076
Niklas <niklasheimburger@gmail.com>
2022-07-15 15:26:37 +0200

    add style-class + new content

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-    const rickApi = 'https://rickandmortyapi.com/api/character';
+const rickApi = 'https://rickandmortyapi.com/api/character';
   fetch(rickApi)
   .then((response) => 
     response.json())
@@ -18,17 +18,35 @@ character.forEach((datas) => {
     characterCard.classList.add("character_card");
   
     const characterName = document.createElement("h2");
+    characterName.classList.add("character_name");
     characterName.innerText = datas.name;
   
-    const characterGender = document.createElement("p");
-    characterGender.innerText = datas.species;
+    const characterList = document.createElement("ul");
+     characterList.classList.add("character_list");
+
+    
+    const characterSpecies = document.createElement('li')
+     characterSpecies.innerText = "species: " + datas.species;
+      characterSpecies.classList.add("character_list_item");
+
+     const characterStatus = document.createElement('li')
+     characterStatus.innerText = "status: " + datas.status;
+     characterStatus.classList.add("character_list_item");
+    
+
+      const characterGender = document.createElement('li')
+      characterGender.innerText = "gender: " + datas.gender;
+      characterGender.classList.add("character_list_item");
 
 
-  //classList for p//
+
+
   
+     // Append: 
     document.body.append(characterCard);
+    characterList.append(characterSpecies,characterStatus,characterGender);
   
-    characterCard.append(characterName,characterGender);
+    characterCard.append(characterName,characterList);
   
   });
   
