17d183566ecbb1408c77794eb558a75fa2ff9f69
Niklas <niklasheimburger@gmail.com>
2022-07-15 14:47:32 +0200

    first progress with test array + add new css.file

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-import 
+/*
+ async function fetchCharacter(){
+
+    try {
+      const response = await fetch('https://rickandmortyapi.com/api/character');
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  
+    return data;
+  };
+  
+  fetchCharacter();
+
+  */
