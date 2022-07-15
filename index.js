6d0e252ef9af6955de51e9744418aa8fe9d55c88
Jonas Worrich <moin@jonas-worrich.de>
2022-07-15 10:50:20 +0200

    Add API-call

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world!');
+async function fetchCharacter() {
+  try {
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+fetchCharacter();
